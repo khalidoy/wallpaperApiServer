42c60f68e792ebe07b158de23811277ddb96f674
Khalid Yakhloufi <kyakhloufi@yahoo.fr>
2022-03-20 15:24:20 +0100

    update

--- respondingServer.js
+++ respondingServer.js
@@ -28,7 +28,7 @@ const server = http.createServer((req, res) => {
 
 server.listen(process.env.PORT || 1000, (error) => {
   if (error) {
-    console.log("erroor");
+    console.log("erroorr");
   } else {
     console.log("listening");
   }
