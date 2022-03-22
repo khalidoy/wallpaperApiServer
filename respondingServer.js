b3df2d796d04e51ae3acc1026e755beb45dbc2cf
Khalid Yakhloufi <kyakhloufi@yahoo.fr>
2022-03-22 22:49:42 +0100

    retuned to heroku

--- respondingServer.js
+++ respondingServer.js
@@ -1,6 +1,6 @@
 const http = require("http");
-const searchScrapper = require("../searchScrapper");
-const clickScrapper = require("../clickScrapper");
+const searchScrapper = require("./searchScrapper");
+const clickScrapper = require("./clickScrapper");
 
 const server = http.createServer((req, res) => {
   var rawQuery = req.url;
